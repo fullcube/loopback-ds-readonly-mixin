96bd3687a26612290cd8c4c1a8f7cd347b540c08
Tom Kirkpatrick <tom@systemseed.com>
2017-04-25 10:27:16 +0200

    style: implement eslint and clean up code

--- test/test.js
+++ test/test.js
@@ -1,10 +1,14 @@
-var loopback = require('loopback');
-var lt = require('loopback-testing');
-var chai = require('chai');
-var expect = chai.expect;
-var TestDataBuilder = lt.TestDataBuilder
-var ref = TestDataBuilder.ref
-var request = require('supertest')
+const lt = require('loopback-testing')
+const chai = require('chai')
+const expect = chai.expect
+const TestDataBuilder = lt.TestDataBuilder
+const ref = TestDataBuilder.ref
+const request = require('supertest')
+
+chai.use(require('dirty-chai'))
+
+// Create a new loopback app.
+const app = require('./fixtures/simple-app/server/server.js')
 
 // Helper function to make api requests.
 function json(verb, reqUrl) {
@@ -13,119 +17,101 @@ function json(verb, reqUrl) {
     .set('Accept', 'application/json')
 }
 
-// Create a new loopback app.
-var app = require('./fixtures/simple-app/server/server.js');
-
 describe('loopback datasource readonly property (mixin sources.js)', function() {
 
-  lt.beforeEach.withApp(app);
+  lt.beforeEach.withApp(app)
 
   describe('Direct', function() {
-    lt.beforeEach.givenModel('Product', {name: 'some book', type: 'book', status: 'pending'}, 'product');
-
+    lt.beforeEach.givenModel('Product', { name: 'some book', type: 'book', status: 'pending' }, 'product')
     describe('Create', function() {
-      it('should save readonly properties on create.', function(done) {
-        expect(this.product.name).to.equal('some book');
-        expect(this.product.type).to.equal('book');
-        expect(this.product.status).to.equal('pending');
-        done();
-      });
-    });
+      it('should save readonly properties on create.', function() {
+        expect(this.product.name).to.equal('some book')
+        expect(this.product.type).to.equal('book')
+        expect(this.product.status).to.equal('pending')
+      })
+    })
 
     describe('Update', function() {
-      it('should change readonly properties on update.', function(done) {
-        var self = this;
-        self.product.name = 'some other book';
-        self.product.status = 'disabled';
-        self.product.save(function(err, p) {
-          expect(err).to.not.exist;
-          expect(p.name).to.equal(self.product.name);
-          expect(p.type).to.equal(self.product.type);
-          expect(p.status).to.equal(self.product.status);
-          done();
-        });
-      });
-    });
-  });
+      it('should change readonly properties on update.', function() {
+        this.product.name = 'some other book'
+        this.product.status = 'disabled'
+        return this.product.save()
+          .then(p => {
+            expect(p.name).to.equal(this.product.name)
+            expect(p.type).to.equal(this.product.type)
+            expect(p.status).to.equal(this.product.status)
+          })
+      })
+    })
+  })
 
   describe('Remote', function() {
     describe('Create', function() {
-      lt.beforeEach.givenModel('Product', {name: 'some book', type: 'book', status: 'pending'}, 'product');
-      it('should not save readonly properties', function(done) {
-        var product = this.product;
-        this.post('/api/products')
+      lt.beforeEach.givenModel('Product', { name: 'some book', type: 'book', status: 'pending' }, 'product')
+      it('should not save readonly properties', function() {
+        return this.post('/api/products')
           .send({
             name: 'test product',
-            status: 'active'
+            status: 'active',
           })
           .expect(200)
-          .end(function(err, res) {
-            expect(err).to.not.exist;
-            expect(res.body.name).to.equal('test product');
-            expect(res.body.status).to.equal('temp');
-            done();
-          });
-      });
+          .then(res => {
+            expect(res.body.name).to.equal('test product')
+            expect(res.body.status).to.equal('temp')
+          })
+      })
     })
 
     describe('updateAttributes', function() {
-      lt.beforeEach.givenModel('Product', {name: 'some book', type: 'book'}, 'product');
-      it('should not change readonly properties on update (single readonly property)', function(done) {
-        var product = this.product;
-        this.put('/api/products/' + product.id)
+      lt.beforeEach.givenModel('Product', { name: 'some book', type: 'book' }, 'product')
+      it('should not change readonly properties on update (single readonly property)', function() {
+        return this.put(`/api/products/${this.product.id}`)
           .send({
             name: 'updated name',
-            status: 'disabled'
+            status: 'disabled',
           })
           .expect(200)
-          .end(function(err, res) {
-            expect(err).to.not.exist;
-            expect(res.body.name).to.equal('updated name');
-            expect(res.body.status).to.equal('temp');
-            done();
-          });
-      });
+          .then(res => {
+            expect(res.body.name).to.equal('updated name')
+            expect(res.body.status).to.equal('temp')
+          })
+      })
 
-      lt.beforeEach.givenModel('Person', {name: 'Tom', status: 'disabled', role: 'user'}, 'person');
-      it('should not change readonly properties on update (multiple readonly properties)', function(done) {
-        var Person = this.person;
-        this.put('/api/people/' + this.person.id)
+      lt.beforeEach.givenModel('Person', { name: 'Tom', status: 'disabled', role: 'user' }, 'person')
+      it('should not change readonly properties on update (multiple readonly properties)', function() {
+        return this.put(`/api/people/${this.person.id}`)
           .send({
             name: 'Tom (edited)',
             status: 'active',
             role: 'user',
           })
           .expect(200)
-          .end(function(err, res) {
-            expect(err).to.not.exist;
-            expect(res.body.name).to.equal('Tom (edited)');
-            expect(res.body.status).to.be.undefined;
-            expect(res.body.role).to.be.undefined;
-            done();
-          });
-      });
-    });
+          .then(res => {
+            expect(res.body.name).to.equal('Tom (edited)')
+            expect(res.body.status).to.be.undefined()
+            expect(res.body.role).to.be.undefined()
+          })
+      })
+    })
 
     describe('patchAttributes', function() {
-      lt.beforeEach.givenModel('Product', {name: 'some book', type: 'book', status: 'disabled'}, 'product');
+      lt.beforeEach.givenModel('Product', { name: 'some book', type: 'book', status: 'disabled' }, 'product')
       it('should not change readonly properties on update (single readonly property)', function() {
-        var product = this.product;
-        return json('patch', '/api/products/' + product.id)
+        return json('patch', `/api/products/${this.product.id}`)
           .send({
             name: 'updated name',
-            status: 'disabled'
+            status: 'disabled',
           })
           .expect(200)
           .then(res => {
-            expect(res.body.name).to.equal('updated name');
-            expect(res.body.status).to.equal('disabled');
-          });
-      });
+            expect(res.body.name).to.equal('updated name')
+            expect(res.body.status).to.equal('disabled')
+          })
+      })
 
-      lt.beforeEach.givenModel('Person', {name: 'Tom', status: 'disabled', role: 'user'}, 'person');
+      lt.beforeEach.givenModel('Person', { name: 'Tom', status: 'disabled', role: 'user' }, 'person')
       it('should not change readonly properties on update (multiple readonly properties)', function() {
-        var Person = this.person;
-        return json('patch', '/api/people/' + this.person.id)
+        return json('patch', `/api/people/${this.person.id}`)
           .send({
             name: 'Tom (edited)',
             status: 'active',
@@ -133,89 +119,88 @@ describe('loopback datasource readonly property (mixin sources.js)', function() 
           })
           .expect(200)
           .then(res => {
-            expect(res.body.name).to.equal('Tom (edited)');
-            expect(res.body.status).to.equal('disabled');
+            expect(res.body.name).to.equal('Tom (edited)')
+            expect(res.body.status).to.equal('disabled')
             expect(res.body.role).to.equal('user')
-          });
-      });
+          })
+      })
 
-      lt.beforeEach.givenModel('AuditTrail', {event: 'edit', user: 'tom'}, 'audittrail');
+      lt.beforeEach.givenModel('AuditTrail', { event: 'edit', user: 'tom' }, 'audittrail')
       it('should not change readonly properties on update (full read only model)', function() {
-        var audittrail = this.audittrail;
-        return json('patch', '/api/audittrails/' + audittrail.id)
+        return json('patch', `/api/audittrails/${this.audittrail.id}`)
           .send({
             event: 'update',
-            user: 'john'
+            user: 'john',
           })
-          .expect(403);
-      });
-    });
+          .expect(403)
+      })
+    })
 
-    lt.beforeEach.givenModel('AuditTrail', {event: 'edit', user: 'tom'}, 'audittrail');
-    it('should not change readonly properties on update (full read only model)', function(done) {
-      var audittrail = this.audittrail;
-      this.put('/api/audittrails/' + audittrail.id)
+    lt.beforeEach.givenModel('AuditTrail', { event: 'edit', user: 'tom' }, 'audittrail')
+    it('should not change readonly properties on update (full read only model)', function() {
+      return json('put', `/api/audittrails/${this.audittrail.id}`)
         .send({
           event: 'update',
-          user: 'john'
+          user: 'john',
         })
         .expect(403)
-        .end(done);
-    });
+    })
 
     describe('upsert_put', function() {
-      lt.beforeEach.givenModel('Product', {name: 'book 1', type: 'book', status: 'pending'}, 'product')
+      lt.beforeEach.givenModel('Product', { name: 'book 1', type: 'book', status: 'pending' }, 'product')
       it('should not change readonly properties with bulk updates', function() {
-        var data = { id: this.product.id, status: 'disabled' }
         return json('put', '/api/products')
-          .send(data)
-          .then(res => app.models.Product.findById(this.product.id))
+          .send({ id: this.product.id, status: 'disabled' })
+          .expect(200)
+          .then(() => app.models.Product.findById(this.product.id))
           .then(product => expect(product.status).to.equal('temp'))
       })
     })
 
     describe('upsert_patch', function() {
-      lt.beforeEach.givenModel('Product', {name: 'book 1', type: 'book', status: 'pending'}, 'product');
+      lt.beforeEach.givenModel('Product', { name: 'book 1', type: 'book', status: 'pending' }, 'product')
       it('should not change readonly properties', function() {
-        var data = { id: this.product.id, status: 'disabled' }
         return json('patch', '/api/products')
-          .send(data)
-          .then(res => app.models.Product.findById(this.product.id))
+          .send({ id: this.product.id, status: 'disabled' })
+          .expect(200)
+          .then(() => app.models.Product.findById(this.product.id))
           .then(product => expect(product.status).to.equal('pending'))
       })
     })
 
     describe('replaceById', function() {
-      lt.beforeEach.givenModel('Product', {name: 'book 1', type: 'book', status: 'pending'}, 'product');
+      lt.beforeEach.givenModel('Product', { name: 'book 1', type: 'book', status: 'pending' }, 'product')
       it('should not change readonly properties', function() {
-        var data = { id: this.product.id, status: 'disabled' }
         return json('post', `/api/products/${this.product.id}/replace`)
-          .send(data)
-          .then(res => app.models.Product.findById(this.product.id))
+          .send({ id: this.product.id, status: 'disabled' })
+          .expect(200)
+          .then(() => app.models.Product.findById(this.product.id))
           .then(product => expect(product.status).to.equal('temp'))
       })
     })
 
     describe('replaceOrCreate', function() {
-      lt.beforeEach.givenModel('Product', {name: 'book 1', type: 'book', status: 'pending'}, 'product');
+      lt.beforeEach.givenModel('Product', { name: 'book 1', type: 'book', status: 'pending' }, 'product')
       it('should not set readonly properties', function() {
         return json('post', '/api/products/replaceOrCreate')
           .send(Object.assign(this.product.toJSON(), { status: 'disabled' }))
-          .then(res => app.models.Product.findById(this.product.id))
+          .expect(200)
+          .then(() => app.models.Product.findById(this.product.id))
           .then(product => expect(product.status).to.equal('temp'))
       })
     })
 
     describe('updateAll', function() {
-      lt.beforeEach.givenModel('Product', {name: 'book 1', type: 'book', status: 'disabled'}, 'book1')
-      lt.beforeEach.givenModel('Product', {name: 'book 2', type: 'book', status: 'pending'}, 'book2')
+      lt.beforeEach.givenModel('Product', { name: 'book 1', type: 'book', status: 'disabled' }, 'book1')
+      lt.beforeEach.givenModel('Product', { name: 'book 2', type: 'book', status: 'pending' }, 'book2')
       it('should not change readonly properties with bulk updates', function() {
         return json('post', '/api/products/update')
-          .query({ where: {type: 'book' }})
+          .query({ where: { type: 'book' } })
           .send({ status: 'disabled' })
-          .then(res => app.models.Product.findById(this.book1.id))
+          .expect(200)
+          .then(() => app.models.Product.findById(this.book1.id))
           .then(book1 => expect(book1.status).to.equal('disabled'))
-          .then(res => app.models.Product.findById(this.book2.id))
+          .then(() => app.models.Product.findById(this.book2.id))
           .then(book2 => expect(book2.status).to.equal('pending'))
       })
     })
@@ -225,8 +210,9 @@ describe('loopback datasource readonly property (mixin sources.js)', function() 
       it('should not change readonly properties with bulk updates', function() {
         return json('post', '/api/products/upsertWithWhere')
           .query({ where: { id: this.product.id } })
+          .expect(200)
           .send({ status: 'active', name: 'book 1 (updated)' })
-          .then(res => app.models.Product.findById(this.product.id))
+          .then(() => app.models.Product.findById(this.product.id))
           .then(product => {
             expect(product.status).to.equal('disabled')
             expect(product.name).to.equal('book 1 (updated)')
@@ -237,25 +223,25 @@ describe('loopback datasource readonly property (mixin sources.js)', function() 
     describe('Update via relationship', function() {
       before(function(done) {
         new TestDataBuilder()
-          .define('person', app.models.Person, { name: 'Tom', status: 'disabled', role: 'user' })
-          .define('product', app.models.Product, { name: 'book 2', type: 'book', status: 'pending', personId: ref('person.id') })
+          .define('person', app.models.Person, {
+            name: 'Tom', status: 'disabled', role: 'user' })
+          .define('product', app.models.Product, {
+            name: 'book 2', type: 'book', status: 'pending', personId: ref('person.id') })
           .buildTo(this, done)
       })
 
-      it('should not change readonly properties with updates via a relationship', function(done) {
-        this.put('/api/people/' + this.person.id + '/products/' + this.product.id)
+      it('should not change readonly properties with updates via a relationship', function() {
+        return this.put(`/api/people/${this.person.id}/products/${this.product.id}`)
           .send({
             name: 'updated name',
-            status: 'disabled'
+            status: 'disabled',
           })
           .expect(200)
-          .end(function(err, res) {
-            expect(err).to.not.exist;
-            expect(res.body.name).to.equal('updated name');
-            expect(res.body.status).to.equal('pending');
-            done()
-          });
-      });
-    });
-  });
-});
+          .then(res => {
+            expect(res.body.name).to.equal('updated name')
+            expect(res.body.status).to.equal('pending')
+          })
+      })
+    })
+  })
+})
